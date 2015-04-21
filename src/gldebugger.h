6a81bcf0109c20c6ee8e4d04fa9ceac8c17d9504
auxilius <auxilius@users.noreply.github.com>
2015-04-21 13:44:24 +0200

    updated interface notes and form

--- src/gldebugger.h
+++ src/gldebugger.h
@@ -5,75 +5,58 @@
 #include <gl/glew.h>
 #include <string>
 
+namespace glv {
+
+/* Initializes the library. Needs to be called before any buffer generation, after OpenGL context creation. */
+int init(std::string workingDir, HGLRC glrcToShare = NULL, HDC dcToShare = NULL);
+
+/* Shows a window */
+void show();
+
+/* Hides a viewing window */
+void close();
+
+/* Adds a texture to the library, Caption and an texture ID must be specified. */
+bool setTexture(std::string caption, GLuint texture);
+
+/* Add a line to the text output list. specify a format string and an array of pointers. Output format control signs: bool(%b), int(%i), unsigned(%u), float(%f), double(%d), char(%c). */
+bool setLine(std::string format, void * data);
+bool setLine(std::string format, void * data[]);
+bool setLine(std::string caption, std::string format, void * data);
+bool setLine(std::string caption, std::string format, void * data[]);
+
+/* Add a vertex cloud rendering form for a model */
+bool setModel(std::string caption, unsigned count, GLuint vertices, GLenum type = GL_FLOAT);
+
+/* Specifyes a conetivity between vertices */
+bool modelEdges(std::string caption, GLenum mode, unsigned count, GLuint indices, GLenum type = GL_UNSIGNED_INT);
+
+/* Assign texture id and coordinates for vertices */
+bool modelTexture(std::string caption, GLuint texture, GLuint coordinates, GLenum type = GL_FLOAT);
+
+
+/* UNDER WORK - note that these may not be working correctly! */ 
+
 #define GLV_COLORMAP_RAINBOW 0
 #define GLV_COLORMAP_BLUERED 1
 
-/* Inicializuje kniznicu - ak nie su zadane parametre, automaticky sa nacitaju kontexty ktore su aktualne nastavene */
-int gldInit(std::string workingDir, HGLRC glrcToShare = NULL, HDC dcToShare = NULL);
+/* Assign a per vertex data array to the vertex buffer */
+bool modelData(std::string caption, float * data, float min = 0.0f, float max = 1.0f, int colorMap = GLV_COLORMAP_RAINBOW );
 
-/* Spusti debuggovacie okno */
-void gldStart();
+/* Assign a per vertex data array to the vertex buffer and create a collor buffer to increase render efficiency */
+bool modelColor(std::string caption, float * data, float min = 0.0f, float max = 1.0f, int colorMap = GLV_COLORMAP_RAINBOW );
 
-/* Vypne debugovacie okno */
-void gldStop();
+/* Assign a per vertex color buffer to the model. */
+bool modelColorBuffer(std::string caption, GLuint colorBuffer );
 
-/* Prida texturu - caption je uzivatelsky popis textury a druhy parameter je jej id */
-bool gldAddTexture(std::string caption, GLuint texture);
+/* Assign a vertex-normal buffer */
+bool modelNomals(std::string caption, GLuint normalBuffer );
 
-/* 
-	Prida riadok do vypisu - format udava co sa ma vypisovat a data je smernik na premennu, ktora sa bude vypisovat. 
-	Dostupne formatovanie je: bool(%b), int(%i), unsigned(%u), float(%f), double(%d), char(%c).
-	Nie je potrebne aby format korespondoval s vlozenymi udajmi, napr. smernik na bool mozeme formatovat ako %i 
-*/
-bool gldAddLine(std::string format, void * data);
-bool gldAddLine(std::string format, void * data[]);
-bool gldAddLine(std::string caption, std::string format, void * data);
-bool gldAddLine(std::string caption, std::string format, void * data[]);
+/* Assign a shader program which will be rendering a model in viewer window */
+bool modelShaderProgram(std::string caption, GLuint shaderProgramId);
 
-/* Prida model a nazve ho podla parametru caption. Posledny parameter je pocet vertexov modelu */
-bool gldAddModel(std::string caption, unsigned count, GLuint vertices, GLenum type = GL_FLOAT);
-
-/* Pridadi data modelu, ktory ma popis caption */
-bool gldAddModelData(std::string caption, float * data, float min = 0.0f, float max = 1.0f, int colorMap = GLV_COLORMAP_RAINBOW );
-
-/* Pridadi data modelu, a vytvori color buffer */
-/* RADSEJ NEPOUZIVAT : UNSECURE */
-bool gldAddModelColor(std::string caption, float * data, float min = 0.0f, float max = 1.0f, int colorMap = GLV_COLORMAP_RAINBOW );
-
-/* Pridadi color buffer modelu, a pomocou neho bude zafarbovat vertexy */
-bool gldAddModelColorBuffer(std::string caption, GLuint colorBuffer );
-
-/* Pridadi normaly modelu, a vizualizuje ich pre kazdy vertex */
-bool gldAddModelNomals(std::string caption, GLuint normalBuffer );
-
-/* Pridadi pole indexov modelu, ktory ma popis caption */
-bool gldAddModelEdges(std::string caption, GLenum mode, unsigned count, GLuint indices, GLenum type = GL_UNSIGNED_INT);
-
-/* Pridadi texturu a koordinaty modelu, ktory ma popis caption */
-bool gldAddModelTexture(std::string caption, GLuint texture, GLuint coordinates, GLenum type = GL_FLOAT);
-
-/* Pre model sa priradi shader program ktory bude upravovat renderovanie */
-bool gldAddModelShaderProgram(std::string caption, GLuint shaderProgramId);
-
-bool gldAddModelVertexAttrib(std::string caption, GLuint atributeID, GLint size, GLenum type, GLuint buffer);
-
-
-
-namespace glv { 
-
-	int init(std::string workingDir, HGLRC glrcToShare = NULL, HDC dcToShare = NULL);
-
-	bool setTexture(std::string caption, GLuint textureID);
-
-	bool setLine(std::string caption, std::string format, void * data);
-
-	bool setModel(std::string caption, unsigned count, GLuint vertices, GLenum type = GL_FLOAT);
-
-	bool setModelData(std::string caption, float * data, float min = 0.0f, float max = 1.0f);
-
-	bool setModelEdges(std::string caption, GLenum mode, unsigned count, GLuint indices, GLenum type = GL_UNSIGNED_INT);
-
-	bool setModelTexture(std::string caption, GLuint texture, GLuint coordinates, GLenum type = GL_FLOAT);
+/* Assign vertex attribute list to the model */
+bool modelVertexAttrib(std::string caption, GLuint atributeID, GLint size, GLenum type, GLuint buffer);
 
 };
 
